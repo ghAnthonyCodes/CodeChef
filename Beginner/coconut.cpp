fbc7ef367f72cba07f98ed05cb321ffe53bfd3ae
ghAnthonyCodes <85413356+ghAnthonyCodes@users.noreply.github.com>
2021-06-14 06:42:45 -0700

    Add files via upload

---
+++ Beginner/coconut.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+int main() {
+  int T;
+  int xa, xb, Xa, Xb;
+  scanf("%d", &T);
+  while (T--) {
+    scanf("%d %d %d %d", &xa, &xb, &Xa, &Xb); 
+    int A = static_cast<int>(ceil(1.0*Xa/xa));
+    int B = static_cast<int>(ceil(1.0*Xb/xb));
+
+    // Solve
+    printf("%d\n", A+B);
+  }
+  return 0;
+}
